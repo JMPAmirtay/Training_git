89cde59c7c0cf227664e5900061ce5218fa3c3c4
Amirtay Dinmukhammed <amirtayd@mail.ru>
2023-03-10 15:34:12 +0600

    fixed upload news image

--- add_news.php
+++ add_news.php
@@ -78,7 +78,7 @@ if (!$_SESSION['user']) {
                         <div class="card">
                             <div class="card-body">
                                 <div class="basic-form">
-                                    <form action="vendor_news/add_news.php" method="post">
+                                    <form action="vendor_news/add_news.php" method="post" enctype="multipart/form-data">
                                         <input type="hidden" name="id" value="<?= $news['id'] ?>">
                                         <div class="mb-3 row">
                                             <label for="title">Заголовок:</label>
@@ -94,60 +94,67 @@ if (!$_SESSION['user']) {
                                             <!-- <div class="card-body custom-ekeditor">
                                                 <div id="ckeditor"></div> -->
                                         </div>
-                                </div>
-                                <div class="mb-3 row">
-                                    <label for="pubdate">Дата публикации:</label>
-                                    <input type="date" class="datepicker-default form-control" id="pubdate" name="pubdate">
-                                </div>
-                                <div class="mb-3 row">
-                                    <label for="author">Автор:</label>
-                                    <select class="default-select  form-control wide">
+                                        <div class="mb-3 row">
+                                            <label for="pubdate">Дата публикации:</label>
+                                            <input type="date" class="datepicker-default form-control" id="pubdate" name="pubdate">
+                                        </div>
+                                        <div class="mb-3 row">
+                                            <label for="author">Автор:</label>
+                                            <select class="default-select  form-control wide">
+                                                <?php
+                                                $redactors = mysqli_query($connect, "SELECT * FROM `redactors`");
+                                                $redactors = mysqli_fetch_all($redactors);
+                                                foreach ($redactors as $redactor) {
+                                                ?>
+                                                    <option><?= $redactor[1] ?></option>
+                                                <?php
+                                                }
+                                                ?>
+                                            </select>
+                                        </div>
+                                        <div class="mb-3 row">
+                                            <label for="image">Изображение:</label>
+                                            <div class="input-group mb-3">
+                                                <span class="input-group-text">Upload</span>
+                                                <div class="form-file">
+                                                    <input type="file" name="news_image" class="form-file-input form-control" accept="image/png, image/gif, image/jpeg">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="mb-3 row">
+                                            <label for="tags">Теги:</label>
+                                            <textarea class="form-control" rows="4" id="tags" name="tags"></textarea>
+                                        </div>
+                                        <div class="mb-3 row">
+                                            <label for="status">Статус:</label>
+                                            <select class="nice-select form-control wide" id="status" name="status">
+                                                <option value="draft">Черновик</option>
+                                                <option value="published">Опубликован</option>
+                                                <option value="archived">Архивирован</option>
+                                            </select>
+                                        </div>
+                                        <div class="mb-3 row">
+                                            <div class="col-sm-10">
+                                                <button type="submit" class="btn btn-primary">Добавить</button>
+                                            </div>
+                                        </div>
                                         <?php
-                                        $redactors = mysqli_query($connect, "SELECT * FROM `redactors`");
-                                        $redactors = mysqli_fetch_all($redactors);
-                                        foreach ($redactors as $redactor) {
+                                        if ($_SESSION['message']) {
                                         ?>
-                                            <option><?= $redactor[1] ?></option>
+                                            <div class="alert alert-warning solid alert-square "><strong>Error! </strong><?= $_SESSION['message'] ?></div>
                                         <?php
                                         }
+                                        unset($_SESSION['message']);
                                         ?>
-                                    </select>
+                                    </form>
                                 </div>
-                                <div class="mb-3 row">
-                                    <label for="image">Изображение:</label>
-                                    <div class="input-group mb-3">
-                                        <span class="input-group-text">Upload</span>
-                                        <div class="form-file">
-                                            <input type="file" class="form-file-input form-control">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="mb-3 row">
-                                    <label for="tags">Теги:</label>
-                                    <textarea class="form-control" rows="4" id="tags" name="tags"></textarea>
-                                </div>
-                                <div class="mb-3 row">
-                                    <label for="status">Статус:</label>
-                                    <select class="nice-select form-control wide" id="status" name="status">
-                                        <option value="draft">Черновик</option>
-                                        <option value="published">Опубликован</option>
-                                        <option value="archived">Архивирован</option>
-                                    </select>
-                                </div>
-                                <div class="mb-3 row">
-                                    <div class="col-sm-10">
-                                        <button type="submit" class="btn btn-primary">Добавить</button>
-                                    </div>
-                                </div>
-                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <!--**********************************
+        <!--**********************************
             Content body end
         ***********************************-->
     </div>
--- vendor_news/add_news.php
+++ vendor_news/add_news.php
@@ -6,11 +6,17 @@ $description = $_POST['description'];
 $text = $_POST['text'];
 $pubdate = $_POST['pubdate'];
 $author = $_POST['author'];
-$image = $_POST['image'];
 $tags = $_POST['tags'];
 $status = $_POST['status'];
 
-mysqli_query($connect, "INSERT INTO `news` (`id`, `Title`, `Description`, `Text`, `Publication Date`, `Author`, `Image`, `Category`, `Tags`, `Views`, `Rating`, `Status`) VALUES (NULL, '$title', '$description', '$text', '$pubdate', '$author', '$image', '', '$tags', NULL, NULL, '$status')");
+
+$path = 'img_archive/' . time() . $_FILES['news_image']['name'];
+if (!move_uploaded_file($_FILES['news_image']['tmp_name'], '../' . $path)) {
+	$_SESSION['message'] = 'Ошибка при загрузке изображения';
+	header('Location: ../page_register.php');
+}
+
+mysqli_query($connect, "INSERT INTO `news` (`id`, `Title`, `Description`, `Text`, `Publication Date`, `Author`, `Image`, `Category`, `Tags`, `Views`, `Rating`, `Status`) VALUES (NULL, '$title', '$description', '$text', '$pubdate', '$author', '$path', '', '$tags', NULL, NULL, '$status')");
 
 /* Перенаправление браузера на другую страницу в той же директории, что и
 	изначально запрошенная */
